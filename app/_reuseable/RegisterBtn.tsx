5cc9f0f6bf8e618f313139c762e6d201a9f6c61e
Ogaranya <enomahog@gmail.com>
2023-09-24 05:25:57 +0100

    reg btn

---
+++ app/_reuseable/RegisterBtn.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+
+const RegisterBtn = () => {
+  return (
+    <Link href="/register" className="bg-gradient-to-r from-pink to-blue px-14 py-3 text-2xl rounded">Register</Link>
+  )
+}
+
+export default RegisterBtn
