39d64c2e6bb3a481161063dc6682f9b5aa99da44
Ogaranya <enomahog@gmail.com>
2023-09-24 05:25:04 +0100

    hero

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -1,7 +1,42 @@
+import Section from "../_reuseable/Section";
+import RegisterBtn from "../_reuseable/RegisterBtn";
+import avatar from "../_assets/avatar.svg"
+
 const Hero = () => {
   return (
-    <section>Hero</section>
-  )
-}
+    <Section>
+      <div className="">
+        <div className="star"></div>
 
-export default Hero
+        <h2 className="">
+          <div className="bulb"></div>
+          getlinked Tech Hackathon <span className="">1.0</span>
+          <div className="chain"></div>
+          <div className="fire"></div>
+        </h2>
+ 
+        <small>
+          Participate in getlinked tech Hackathon 2023 stand a chance to win a
+          Big prize
+        </small>
+
+        <RegisterBtn />
+
+        <div className="star"></div>
+
+        <div className="">
+            <div className="">00 <small className="">H</small></div>
+            <div className="">00 <small className="">M</small></div>
+            <div className="">00 <small className="">S</small></div>
+        </div>
+      </div>
+      <div className="">
+
+        <h3>Igniting a Revolution in HR Innovation</h3>
+
+      </div>
+    </Section>
+  );
+};
+
+export default Hero;
