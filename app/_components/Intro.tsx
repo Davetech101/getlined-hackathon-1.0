ee56aff8054e01844c3bab36fb02d322c8094e15
Ogaranya <enomahog@gmail.com>
2023-10-01 00:10:54 +0100

    intro

---
+++ app/_components/Intro.tsx
@@ -0,0 +1,14 @@
+import Section from "../_reuseable/Section";
+
+const Intro = () => {
+  return (
+    <Section>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto unde
+      dolorem vitae nulla eius deserunt expedita libero debitis praesentium
+      cumque officiis, fugiat laboriosam quibusdam sint dolor delectus.
+      Officiis, nesciunt reiciendis.
+    </Section>
+  );
+};
+
+export default Intro;
