c14ba788166c49d3ac099475c519bbde5ce7dec3
Ogaranya <enomahog@gmail.com>
2023-10-01 00:08:37 +0100

    sec

--- app/_reuseable/Section.tsx
+++ app/_reuseable/Section.tsx
@@ -2,7 +2,7 @@
 type secProps = { children: React.ReactNode };
 const Section = ({ children }: secProps) => {
   return (
-    <section className="flex items-center justify-between py-20">
+    <section className="flex items-center justify-between py-20 border-b border-border">
         {children}
     </section>
   )
