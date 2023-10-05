6c47232bc9faee7a8f6518b99a9f423db66e987b
Ogaranya <enomahog@gmail.com>
2023-10-05 04:03:34 +0100

    timeline

--- app/_components/Timeline.tsx
+++ app/_components/Timeline.tsx
@@ -1,63 +1,75 @@
+import { useCallback } from "react";
 import Main from "../_reuseable/Main";
 import Section from "../_reuseable/Section";
 
 const Timeline = () => {
   const timeline = [
     {
-      id: "1",
+      id: 1,
       name: "Hackathon Announcement",
       txt: "The getlinked tech hackathon 1.0 is formally announced to the general public and teams begin to get ready to register",
       date: "November 18, 2023",
     },
 
     {
-      id: "2",
+      id: 2,
       name: "Teams Registration begins",
       txt: "Interested teams can now show their interest in the getlinked tech hackathon 1.0 2023 by proceeding to register",
       date: "November 18, 2023",
     },
     {
-      id: "3",
+      id: 3,
       name: "Teams Registration ends",
       txt: "Interested Participants are no longer Allowed to register",
       date: "November 18, 2023",
     },
     {
-      id: "4",
-      name: "",
-      txt: "",
-      date: "November 18, 2023",
-    },
-    {
-      id: "5",
+      id: 4,
       name: "Announcement of the accepted teams and ideas",
       txt: "All teams whom idea has been accepted into getlinked tech hackathon 1.0 2023 are formally announced",
       date: "November 18, 2023",
     },
     {
-      id: "6",
+      id: 5,
+      name: "Getlinked Hackathon 1.0 Offically Begins",
+      txt: "Accepted teams can now proceed to build their ground breaking skill driven solutions",
+      date: "November 18, 2023",
+    },
+    {
+      id: 6,
       name: "Demo Day",
       txt: "Teams get the opportunity to pitch their projects to judges. The winner of the hackathon will also be announced on this day",
       date: "November 18, 2023",
     },
-    {
-      id: "",
-      name: "",
-      txt: "",
-      date: "",
-    },
   ];
+
+  const line = timeline.map((line) => {
+    return (
+      <div key={line.id} className={`flex items-center justify-center gap-24  ${line.id % 2 === 0 ? "flex-row-reverse text-left": "flex-row text-right"}`}>
+        <div className=" w-4/12">
+          <h3 className="text-2xl text-pink">{line.name}</h3>
+          <p className="text-xl">{line.txt}</p>
+        </div>
+
+        <div className="bg-gradient-to-r from-pink to-blue w-10 h-10 rounded-full text-center flex items-center justify-center">{line.id}</div>
+
+        <p className={`text-2xl text-pink w-4/12 text-left ${line.id % 2 === 0 ? "text-left" : "text-right"}`}>{line.date}</p>
+      </div>
+    );
+  });
 
   return (
     <Section>
-      <Main>
-        <h3 className="">Timeline</h3>
+        <main className="relative py-24 px-32 flex items-center flex-col justify-center w-full">
+          <h3 className="text-4xl mb-6">Timeline</h3>
 
-        <p>
-          Here is the breakdown of the time we anticipate using for the upcoming
-          event.
-        </p>
-      </Main>
+          <p className="text-xl w-3/6 text-center mb-24">
+            Here is the breakdown of the time we anticipate using for the
+            upcoming event.
+          </p>
+
+          {line}
+        </main>
     </Section>
   );
 };
