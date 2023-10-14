f2d330c4245dffc74586eff9b1b7cd3b3cd4afcb
Ogaranya <enomahog@gmail.com>
2023-10-14 21:42:05 +0100

    privacy

--- app/_components/Privacy.tsx
+++ app/_components/Privacy.tsx
@@ -18,7 +18,30 @@ const Privacy = () => {
           </div>
 
           <div className="">
-            
+            <p className="">
+              At getlinked tech Hackathon 1.0, we value your privacy and are
+              committed to protecting your personal information. This Privacy
+              Policy outlines how we collect, use, disclose, and safeguard your
+              data when you participate in our tech hackathon event. By
+              participating in our event, you consent to the practices described
+              in this policy.
+            </p>
+
+            <div className="">
+              <h6 className="">Licensing Policy</h6>
+              <p className="">Here are terms of our Standard License:</p>
+            </div>
+
+            <div className="">
+              <div className="">
+                The Standard License grants you a non-exclusive right to
+                navigate and register for our event
+              </div>
+              <div className="">
+                You are licensed to use the item available at any free source
+                sites, for your project developement
+              </div>
+            </div>
           </div>
         </div>
         <div className=""></div>
