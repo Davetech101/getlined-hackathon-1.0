6a699bcfd985562f60ede0ddd1feaf37b0b18d04
Ogaranya <enomahog@gmail.com>
2023-10-16 01:07:40 +0100

    privacy

--- app/_components/Privacy.tsx
+++ app/_components/Privacy.tsx
@@ -6,20 +6,18 @@ const Privacy = () => {
   return (
     <Section>
       <main className="py-32 px-32 flex items-center justify-between">
-        <div className="">
-          <div className="">
+        <div className="w-2/4">
             <H3 head="Privacy Policy and" span="Terms" />
 
-            <p>Last updated on September 12, 2023</p>
+            <p className="mb-8 text-xl">Last updated on September 12, 2023</p>
 
-            <p className="">
+            <p className="text-xl mb-20 pr-72">
               Below are our privacy & policy, which outline a lot of goodies.
               it’s our aim to always take of our participant
             </p>
-          </div>
 
-          <div className="">
-            <p className="">
+          <div className="border border-pink p-24">
+            <p className="text-xl mb-8">
               At getlinked tech Hackathon 1.0, we value your privacy and are
               committed to protecting your personal information. This Privacy
               Policy outlines how we collect, use, disclose, and safeguard your
@@ -28,45 +26,52 @@ const Privacy = () => {
               in this policy.
             </p>
 
-            <div className="">
-              <h6 className="">Licensing Policy</h6>
-              <p className="">Here are terms of our Standard License:</p>
+            <div className="mb-8">
+              <h6 className="text-pink text-xl">Licensing Policy</h6>
+              <p className="text-xl">Here are terms of our Standard License:</p>
             </div>
 
-            <div className="">
-              <div className="">
-              <Image
-              src={"/_assets/check.svg"}
-              alt={"check"}
-              width={20}
-              height={20}
-              className=""
-            />
-                <p className="">The Standard License grants you a non-exclusive right to
-                navigate and register for our event</p>
+              <div className="flex items-start gap-10 mb-8">
+                <Image
+                  src={"/_assets/check.svg"}
+                  alt={"check"}
+                  width={20}
+                  height={20}
+                  className=""
+                />
+                <p className="text-xl">
+                  The Standard License grants you a non-exclusive right to
+                  navigate and register for our event
+                </p>
               </div>
-              <div className="">
-              <Image
-              src={"/_assets/check.svg"}
-              alt={"check"}
-              width={20}
-              height={20}
-              className=""
-            />
-                <p className="">You are licensed to use the item available at any free source
-                sites, for your project developement</p>
+              <div className="flex items-start gap-10">
+                <Image
+                  src={"/_assets/check.svg"}
+                  alt={"check"}
+                  width={20}
+                  height={20}
+                  className=""
+                />
+                <p className="text-xl">
+                  You are licensed to use the item available at any free source
+                  sites, for your project developement
+                </p>
               </div>
-            </div>
+
+              <button className="mx-auto mt-12 block bg-gradient-to-r from-pink to-blue px-14 py-3 text-2xl rounded">
+                Read More
+              </button>
           </div>
         </div>
         <div className="">
-        <Image
-              src={"/_assets/privacy.svg"}
-              alt={"privacy"}
-              width={200}
-              height={200}
-              className=""
-            /><Image
+          <Image
+            src={"/_assets/privacy.svg"}
+            alt={"privacy"}
+            width={200}
+            height={200}
+            className=""
+          />
+          <Image
             src={"/_assets/privacy.png"}
             alt={"lock"}
             width={200}
