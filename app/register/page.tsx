ee215baae2af16709465da7fca7f82e380597530
Ogaranya <enomahog@gmail.com>
2023-10-17 04:40:08 +0100

    register

---
+++ app/register/page.tsx
@@ -0,0 +1,13 @@
+import Header from "../_components/Header"
+import Register from "../_components/Register"
+
+const page = () => {
+  return (
+    <main>
+        <Header/>
+        <Register/>
+    </main>
+  )
+}
+
+export default page
